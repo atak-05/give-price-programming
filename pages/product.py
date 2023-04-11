5599c9ed92d4aa625ff0d9b852fb8e29ddea9118
gizem <gizemtuncer17@gmail.com>
2023-04-11 22:59:59 +0300

    backend was working (product choise)

--- pages/product.py
+++ pages/product.py
@@ -16,12 +16,13 @@ class ProductPage(QWidget):
                 }
         self.productform = Ui_product()
         self.productform.setupUi(self,urun_listesi)
+        
     #     self.productform.pushButton_product_type.clicked.connect(self.productColor)
     #     self.productform.pushButton_2_product_color.clicked.connect(self.productNumber)
     #     self.productform.pushButton_3_product_number.clicked.connect(self.sonuc)
     #     self.sonucform = SonucPage()
     # def productColor(self):
-    #     self.productform.stackedWidget.setCurrentIndex(1)
+    #      self.productform.stackedWidget.setCurrentIndex(1)
     # def productNumber(self):
     #     self.productform.stackedWidget.setCurrentIndex(2)
     # def sonuc(self):
--- pages_ui/deneme.py
+++ pages_ui/deneme.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtWidgets
 
 
 class Ui_product(object):
+   
     def setupUi(self, product, urun_listesi):
         product.setObjectName("product")
         product.resize(606, 300)
@@ -11,32 +12,48 @@ class Ui_product(object):
         self.stackedWidget.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.stackedWidget.setObjectName("stackedWidget")
 
-        # Product type page
+        # Product type page---------------------------------------------------------------------------------------
         self.page_product_type = QtWidgets.QWidget()
         self.page_product_type.setObjectName("page_product_type")
-        h_layout = QtWidgets.QHBoxLayout(self.page_product_type)
-        h_layout.setContentsMargins(0, 0, 0, 0)
-        h_layout.setSpacing(10)
+        self.h_layout = QtWidgets.QHBoxLayout(self.page_product_type)
+        self.h_layout.setContentsMargins(0, 0, 0, 0)
+        self.h_layout.setSpacing(10)
 
         # Create QPushButton for each product in the product list and add it to the layout
         for urun in urun_listesi:
             button = QtWidgets.QPushButton(urun)
             button.setObjectName(urun)
-            h_layout.addWidget(button)
+            self.h_layout.addWidget(button)
+            print("1.adım  "+button.objectName())
+        for button in self.page_product_type.findChildren(QtWidgets.QPushButton):
+            button.clicked.connect(self.on_product_type_button_clicked)
 
         # Set layout for the product type page and add it to the stacked widget
-        self.page_product_type.setLayout(h_layout)
+        self.page_product_type.setLayout(self.h_layout)
         self.stackedWidget.addWidget(self.page_product_type)
-
-        # Product color page
+        
+         # Product color page--------------------------------------------------------------------------------------
         self.page_2_product_color = QtWidgets.QWidget()
         self.page_2_product_color.setObjectName("page_2_product_color")
-        self.pushButton_2_product_color = QtWidgets.QPushButton(self.page_2_product_color)
-        self.pushButton_2_product_color.setGeometry(QtCore.QRect(10, 50, 75, 23))
-        self.pushButton_2_product_color.setObjectName("pushButton_2_product_color")
-        self.stackedWidget.addWidget(self.page_2_product_color)
+        self.color_layout = QtWidgets.QVBoxLayout(self.page_2_product_color)
+        self.color_layout.setContentsMargins(0, 0, 0, 0)
+        self.color_layout.setSpacing(10)
 
-        # Product number page
+        # Set layout for the product color page and add it to the stacked widget
+        self.page_2_product_color.setLayout(self.color_layout)
+        self.stackedWidget.addWidget(self.page_2_product_color)
+      
+        for urun in urun_listesi:
+            button = QtWidgets.QPushButton(urun)
+            button.setObjectName(urun)
+            self.h_layout.addWidget(button)
+            print("1.adım  "+button.objectName())
+        for button in self.page_product_type.findChildren(QtWidgets.QPushButton):
+            button.clicked.connect(self.on_product_type_button_clicked)
+
+
+
+        # Product number page************************************************
         self.page_3_product_number = QtWidgets.QWidget()
         self.page_3_product_number.setObjectName("page_3_product_number")
         self.pushButton_3_product_number = QtWidgets.QPushButton(self.page_3_product_number)
@@ -50,15 +67,61 @@ class Ui_product(object):
         # Connect signals and slots
         # self.retranslateUi(product)
         QtCore.QMetaObject.connectSlotsByName(product)
+    def on_product_type_button_clicked(self):
+        button = self.page_product_type.sender()
+        product_name = button.objectName()
 
+        # Clear any existing color buttons from the color page
+        for color_button in self.page_product_color.findChildren(QtWidgets.QPushButton):
+            self.color_layout.removeWidget(color_button)
+            color_button.deleteLater()
+
+        # Get the colors for the selected product and create a QPushButton for each color
+        colors = self.get_colors_for_product(product_name)
+        for color in colors:
+            color_button = QtWidgets.QPushButton(color)
+            color_button.setObjectName(color)
+            self.color_layout.addWidget(color_button)
+            print("2.adım  "+color_button.objectName())
+
+        # Set the current index of the stacked widget to the color page
+        self.stackedWidget.setCurrentIndex(1)
+    def get_colors_for_product(product_name):
+        if product_name ==urun_listesi[product_name] :
+            return ["Red", "Blue", "Green"]
+        elif product_name == "Jeans":
+            return ["Black", "Blue", "Gray"]
+        elif product_name == "Shoes":
+            return ["Brown", "Black", "White"]
+        else:
+            return []
+        
+    def renk_secimi(self,urun):
+            for color in urun_listesi[urun]["renkler"]:
+                print("********kalemin color************" + color)
+                button = QtWidgets.QPushButton(color)
+                button.setObjectName(color)
+                self.h_layout.addWidget(button)
+                print("2.adım  "+button.objectName())
+            for button in self.page_product_type.findChildren(QtWidgets.QPushButton):
+                button.clicked.connect(self.on_product_type_button_clicked)
+            self.page_product_type.setLayout(self.h_layout)
+            self.stackedWidget.addWidget(self.page_2_product_color)
+            
+    
+
+        # Do something with the product name, such as showing the next page for selecting colors
     def retranslateUi(self, product):
         _translate = QtCore.QCoreApplication.translate
         product.setWindowTitle(_translate("product", "Form"))
-        self.pushButton_product_type.setText(_translate("product", "ürün type"))
-        self.pushButton_2_product_color.setText(_translate("product", "ürün rengi"))
-        self.pushButton_3_product_number.setText(_translate("product", "ürün adedi"))
+ 
 
-urun_listesi = { }
+urun_listesi = {
+                    "kalem": {"renkler": {"mavi": 3, "siyah": 5, "kırmızı": 4}, "fiyat": None},
+                    "defter": {"renkler": {"sarı": 8, "pembe": 10, "yeşil": 9}, "fiyat": None},
+                    "kalemlik": {"renkler": {"mor": 6, "turuncu": 7, "beyaz": 8}, "fiyat": None},
+                    "bardak": {"renkler": {"şeffaf": 2, "mavi": 3, "kırmızı": 4}, "fiyat": None}
+                }
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
